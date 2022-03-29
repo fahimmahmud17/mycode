7000c5eef40cbb3bd9b0d3519fc13a1e7478037e
Fahim Mahmud <101657129+fahimbug@users.noreply.github.com>
2022-03-29 21:32:08 +0600

    Add files via upload
    This code prints Hello World.

---
+++ hello world.c
@@ -0,0 +1,21 @@
+/* beecrowd | 1000
+Hello World!
+Jean Bez, beecrowd  Brasil
+
+Timelimit: 1
+
+
+Welcome to beecrowd!
+
+Your first program in any programming language is usually "Hello World!". In this first problem all you have to do is print this message on the screen.
+
+Input
+This problem has no input.
+
+Output
+You must print the message Hello World! and then the endline as shown below. */
+#include <stdio.h>
+int main(){
+    printf("Hello World!\n");
+    return 0;
+}
