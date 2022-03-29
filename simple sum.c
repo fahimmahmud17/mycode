21c15e04d62ae7fd78b7fc9292b15ede4e929022
Fahim Mahmud <101657129+fahimbug@users.noreply.github.com>
2022-03-29 21:59:17 +0600

    Add files via upload
    Read two integer values, in this case, the variables A and B. After this, calculate the sum between them and assign it to the variable SOMA. Write the value of this variable.

---
+++ simple sum.c
@@ -0,0 +1,24 @@
+/*beecrowd | 1003
+Simple Sum
+Adapted by Neilor Tonin, URI  Brazil
+
+Timelimit: 1
+Read two integer values, in this case, the variables A and B. After this, calculate the sum between them and assign it to the variable SOMA. Write the value of this variable.
+
+Input
+The input file contains 2 integer numbers.
+
+Output
+Print the message "SOMA" with all the capital letters, with a blank space before and after the equal signal followed by the corresponding value to the sum of A and B. Like all the problems, don't forget to print the end of line, otherwise you will receive "Presentation Error"
+*/
+#include<stdio.h>
+
+int main()
+{
+int A,B;
+scanf("%d",&A);
+scanf("%d",&B);
+
+printf("SOMA = %d\n",A+B);
+return 0;
+}
